254c7bc4f1c415c9c149e153f4927770034d7b87
Sean Hogan <shogun70@gmail.com>
2011-02-04 23:10:09 +1100

    Split out page diagnostics utils into page-diag-workers.html This could facilitate their placement in a pop-up window, as many of the browser debugging tools allow.

--- test/page-basic.js
+++ test/page-basic.js
@@ -1,6 +1,4 @@
-MathJaxDx.Basic = (function() {	
-
-var checks = [
+MathJaxDx.Basic.checks = [
 {
 name: "Startup",
 title: "MathJax Startup",
@@ -70,70 +68,4 @@ checks: [
 ]
 }
 ];
-
-var content = [
-'<table id="basic">',
-'	<caption>Basic troubleshooting checks</caption>',
-'	<col style="width: 4em;" />',
-'	<col />',
-'	<tbody class="template">',
-'		<tr><th style="white-space: nowrap;"></th><td></td></tr>',
-'		<tr><td></td><td></td></tr>',
-'	</tbody>',
-'</table>'
-].join("\n");
-
-var displayElement, $table, $tBodyTemplate;
-
-var init = function(el) {
-	displayElement = el;
-	var $display = $(el);
-	$display.html(content);
-	$table = $display.find("table");
-	$tBodyTemplate = $table.find("tbody.template");
-	$tBodyTemplate.remove().removeClass("template");
-}
-
-var runChecks = function(context, MathJax) {
-	Array.forEach(checks, function(checkGroup) {
-		var $tBody = $tBodyTemplate.clone(true);
-		$table.append($tBody);
-		var $rows = $tBody.find("tr");
-		var $titleRow = $rows.eq(0);
-		var $template = $rows.eq(1);
-		$template.remove();
-		$titleRow.find("th, td").first().html(checkGroup.title);
-		
-		Array.forEach(checkGroup.checks, function(check) {
-			var $row = $template.clone(true), $cells = $row.find("th, td");
-			$cells.eq(1).html(check.title);
-			$cells.eq(0).html('<input type="checkbox" name="check" readonly />')
-				.find("input").each(function() {
-					this.value = check.name;
-					this.checked = check.test(context, MathJax);
-				});
-			$tBody.append($row);
-		})
-	});
-}
-
-
-return {
-
-init: init,
-
-onPageReady: function(context) {
-	
-},
-
-onMathJaxReady: function(context, MathJax) {
-	MathJax.Hub.Register.StartupHook("End", function() {	
-		runChecks(context, MathJax);
-	});	
-}
-
-}
-
-
-})();
 
